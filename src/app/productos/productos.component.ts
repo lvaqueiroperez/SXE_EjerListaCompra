68025896fee72fdfdbb0e017627540fb7e187947
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-02-18 20:16:38 +0100

    "creada la función que usará el evento "click" 2 junto a su variable"

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -11,6 +11,7 @@ import {PRODUCTOSSELEC_ARRAY} from '../Clases/productosSelecArray';
 })
 export class ProductosComponent implements OnInit {
   selectedProducto: Productos;
+  selectedProducto2: Productos;
 
   productosA = PRODUCTOS_ARRAY;
 
@@ -22,12 +23,23 @@ export class ProductosComponent implements OnInit {
   ngOnInit() {
 
   }
+
   // función que usaremos en el evento "click" para asignar a la variable "selectedProducto" el producto seleccionado
   onSelect(producto: Productos, index): void {
     this.selectedProducto = producto;
     producto.comprado = true;
     this.productosSelecA.push(producto);
     this.productosA.splice(index, 1);
+
+  }
+
+  // función usada por el segundo evento "click" para asignar a la variable "selectedProducto2" el producto seleccionado
+  // esta función es usada con los productos seleccionados
+  onSelect2(producto: Productos, index): void {
+    this.selectedProducto2 = producto;
+    producto.comprado = false;
+    this.productosA.push(producto);
+    this.productosSelecA.splice(index, 1);
 
   }
 
