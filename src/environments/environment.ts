eded52be788fb12a3ad12adb4273832d5e323d6a
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-03-11 20:48:27 +0100

    "nueva rama para usar firebase"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {config} from 'rxjs';
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB0ajKM0zM_dQLXwDuP1jyKszXP_sPizVM',
+    authDomain: 'listacompra-aa800.firebaseapp.com',
+    databaseURL: 'https://listacompra-aa800.firebaseio.com',
+    projectId: 'listacompra-aa800',
+    storageBucket: 'listacompra-aa800.appspot.com',
+    messagingSenderId: '1041483136323',
+    appId: '1:1041483136323:web:1ea205576787148ab443f6',
+    measurementId: 'G-VVH0RPV20E'
+  },
 };
 
 /*
